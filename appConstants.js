56694e31dbcfe47c13d4893a8df354a7a79bd925
maddscorpian <maddscorpian@gmail.com>
2021-09-23 18:41:44 +0530

    First commit

---
+++ appConstants.js
@@ -0,0 +1,6 @@
+const APP_CONSTANTS = {
+    MONGO_DB_CON_STR : "mongodb+srv://admin:ItCounts123@cluster0.6cysz.mongodb.net/test",
+    DB_NAME : "it-counts-db"
+};
+
+module.exports = {APP_CONSTANTS};
